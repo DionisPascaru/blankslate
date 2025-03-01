b4641a7dfa6325cd55d0fbbaf27ec07735223d97
DionisPascaru <denis.pascaru95@gmail.com>
2025-03-01 16:53:25 +0200

    create contacts section

--- landing-page/landing-page.php
+++ landing-page/landing-page.php
@@ -10,6 +10,7 @@
 <?php get_template_part('landing-page/section/timeline'); ?>
 <?php get_template_part('landing-page/section/franchise'); ?>
 <?php get_template_part('landing-page/section/career'); ?>
+<?php get_template_part('landing-page/section/contacts'); ?>
 
 <!--libraries scripts-->
 <script src="<?php echo get_template_directory_uri(); ?>/node_modules/bootstrap/dist/js/bootstrap.min.js'"></script>
