d276a9dcde782ec38831e4457ab525a3ffde60f8
DionisPascaru <denis.pascaru95@gmail.com>
2025-03-02 15:09:31 +0200

    create fixed buttons

---
+++ landing-page/section/fixed-buttons.php
@@ -0,0 +1,24 @@
+<?php ?>
+<section>
+    <div class="rm-fixed-buttons">
+        <div class="container">
+            <div class="rm-fixed-buttons-items">
+                <div class="rm-fixed-button">
+                    <a href="<?php home_url(); ?>" type="button" class="rm-fixed-btn">
+                        <i class="icon-home"></i>
+                    </a>
+                </div>
+                <div class="rm-fixed-button">
+                    <a href="https://b154051.alteg.io/" type="button" class="rm-fixed-btn">
+                        <span>Programare</span>
+                    </a>
+                </div>
+                <div class="rm-fixed-button">
+                    <a href="tel:+37360972777" type="button" class="rm-fixed-btn">
+                        <i class="icon-phone"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
