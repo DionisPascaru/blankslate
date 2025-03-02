8a763475cc613d6e82bd37bec6e198e0fdcee2fe
DionisPascaru <denis.pascaru95@gmail.com>
2025-03-02 14:25:22 +0200

    create footer section

--- footer.php
+++ footer.php
@@ -1,12 +1,40 @@
 </main>
-<?php get_sidebar(); ?>
+<?php //get_sidebar(); ?>
 </div>
 <footer id="footer" role="contentinfo">
-<div id="copyright">
-&copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-</div>
+    <div class="container">
+        <div class="rm-footer">
+            <div class="rm-footer-logo">
+                <div id="lottieRealmenFooter" class="rm-footer-animation"></div>
+            </div>
+            <div class="rm-footer-media">
+                <div class="rm-footer-media-item">
+                    <a href="https://www.facebook.com/realmen.md"><i class="icon-facebook"></i></a>
+                </div>
+                <div class="rm-footer-media-item">
+                    <a href="https://www.instagram.com/realmen.md/"><i class="icon-instagram"></i></a>
+                </div>
+                <div class="rm-footer-media-item">
+                    <a href="https://www.tiktok.com/@realmen.md"><i class="icon-tick-tock"></i></a>
+                </div>
+            </div>
+        </div>
+        <div id="copyright" class="rm-copyright">
+            &copy; 2025 Designed by <strong>REABOV STUDIO</strong>
+        </div>
+    </div>
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            lottie.loadAnimation({
+                container: document.getElementById('lottieRealmenFooter'),
+                renderer: 'svg',
+                loop: true,
+                autoplay: true,
+                path: '<?php echo get_template_directory_uri(); ?>/assets/lottie/realmen.json'
+            });
+        })
+    </script>
 </footer>
 </div>
-<?php wp_footer(); ?>
 </body>
 </html>
