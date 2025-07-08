718a0fbedce9d88fdbe8eee8e7c6a0d72259d24e
DionisPascaru <denis.pascaru95@gmail.com>
2025-07-09 00:37:35 +0300

    Add open graph

--- header.php
+++ header.php
@@ -3,6 +3,22 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width">
+
+    <meta property="og:title" content="REALMEN – Barbershop Chișinău"/>
+    <meta property="og:description" content="Barbershop Chișinău — Creat de bărbați adevărați, pentru bărbați adevărați. Din 2017 rămânem fideli miusiunii noastre!"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="https://realmen.md/"/>
+    <meta property="og:site_name" content="REALMEN Barbershop"/>
+    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/assets/opengraph-logo.jpg"/>
+    <meta property="og:image:width" content="768"/>
+    <meta property="og:image:height" content="512"/>
+    <meta property="og:image:alt" content="REALMEN LOGO"/>
+    <meta property="og:locale" content="ro_RO"/>
+    <meta name="twitter:card" content="summary_large_image"/>
+    <meta name="twitter:title" content="REALMEN – Barbershop Chișinău"/>
+    <meta name="twitter:description" content="Barbershop Chișinău — Creat de bărbați adevărați, pentru bărbați adevărați. Din 2017 rămânem fideli miusiunii noastre!"/>
+    <meta name="twitter:image" content="<?php echo get_template_directory_uri(); ?>/assets/opengraph-logo.jpg"/>
+
     <?php if (is_front_page()) : ?>
         <meta name="description" content="Barbershop Chișinău — Creat de bărbați adevărați, pentru bărbați adevărați. Din 2017 rămânem fideli miusiunii noastre!">
     <?php endif; ?>
